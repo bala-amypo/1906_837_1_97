1390430f003f297c5398480c784d5b65c9652650
bala-amypo <bala@amypo.in>
2025-12-18 17:50:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CertificateService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Certificate;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.CertificateRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CertificateService {
+
+    private final CertificateRepository repository;
+
+    public CertificateService(CertificateRepository repository) {
+        this.repository = repository;
+    }
+
+    public Certificate save(Certificate certificate) {
+        return repository.save(certificate);
+    }
+
+    public List<Certificate> getAll() {
+        return repository.findAll();
+    }
+
+    public Certificate getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Certificate not found"));
+    }
+}
---
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public Student save(Student student) {
+        return repository.save(student);
+    }
+
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+}
