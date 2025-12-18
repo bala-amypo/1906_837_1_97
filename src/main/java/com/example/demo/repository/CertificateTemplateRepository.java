b46f4bcc31553437e7ee04ecbb5a063e6843a4e6
bala-amypo <bala@amypo.in>
2025-12-18 17:04:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/CertificateTemplateRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.CertificateTemplate;
+
+public interface CertificateTemplateRepository extends JpaRepository<CertificateTemplate, Long> {
+}
