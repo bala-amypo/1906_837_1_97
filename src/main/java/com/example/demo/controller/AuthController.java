bf20009fee33621cd94b4c51de53ff87189a1012
bala-amypo <bala@amypo.in>
2025-12-19 05:03:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,19 +1,24 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.*;
-import org.springframework.web.bind.annotation.*;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest req) {
+    public AuthResponse login(@RequestBody AuthRequest request) {
         return new AuthResponse("dummy-token");
     }
 
     @PostMapping("/register")
-    public String register(@RequestBody RegisterRequest req) {
+    public String register(@RequestBody RegisterRequest request) {
         return "User registered";
     }
 }
