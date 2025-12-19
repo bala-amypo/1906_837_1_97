15d5db9cf6be9e218be1554f634c1f8d5f5b140b
bala-amypo <bala@amypo.in>
2025-12-19 08:28:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentServiceImpl(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Student addStudent(Student student) {
+
+        if (repository.findByEmail(student.getEmail()).isPresent()
+                || repository.findByRollNumber(student.getRollNumber()).isPresent()) {
+            throw new RuntimeException("Student email exists");
+        }
+
+        return repository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found"));
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           BCryptPasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public User register(User user) {
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        if (user.getRole() == null) {
+            user.setRole("STAFF");
+        }
+
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+}
